59a8bf464f5694eace3d452b6e8cd6ff9345397f
admin2 <kms30922@naver.com>
2023-01-19 00:29:52 +0900

    coplit-변수와타입

---
+++ 04_getRunCatDistance.js
@@ -0,0 +1,6 @@
+//속력 speed , 시간 time 이 숫자로 주어졌을 때, 이동한 거리를 변수 distance에 할당하여 리턴하는 getRunCatDistance 함수를 작성
+function getRunCatDistance(speed, time) {
+    let distance;
+    distance = speed * time;
+    return distance;
+}
