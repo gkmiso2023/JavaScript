c92536355e71992518576cf21905398ca7515624
admin2 <kms30922@naver.com>
2023-01-19 00:30:47 +0900

    coplit-변수와타입

--- 03_expression.js
+++ 03_expression.js
@@ -1,4 +1,4 @@
-//변수 num1에 숫자 5를 할당하고, 변수 num2에 숫자 7을 할당한 후, 변수 result에 숫자 num1과 숫자 num2의 곱을 할당합니다.
+//변수 num1에 숫자 5를 할당하고, 변수 num2에 숫자 7을 할당한 후, 변수 result에 숫자 num1과 숫자 num2의 곱을 할당
 let num1, num2, result;
 num1 = 5;
 num2 = 7;
