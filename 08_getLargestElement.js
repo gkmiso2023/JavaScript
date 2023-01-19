58f6a9440fe14a3840761ab7c9d79f6f48a19f96
admin2 <kms30922@naver.com>
2023-01-19 21:21:35 +0900

    coplit-배열

---
+++ 08_getLargestElement.js
@@ -0,0 +1,10 @@
+//배열을 입력받아 가장 큰 요소를 리턴
+function getLargestElement(arr) {
+    let max = arr[0];
+    for (let el of arr) {
+      if (el > max) {
+        max = el;
+      }
+    }
+    return max;
+  }
