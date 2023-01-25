a911f9a6adfcf4db6fdb7c7ee4622d4503af5c63
admin2 <kms30922@naver.com>
2023-01-25 14:17:58 +0900

    coplit-객체

---
+++ 03_addPropertyAndValue.js
@@ -0,0 +1,6 @@
+//객체와 키, 값을 입력받아 키에 값을 할당
+//별도의 리턴문(return statement)을 작성하지 않습니다.
+//이미 정의된 속성에 해당하는 키 값을 입력받은 경우, 입력받은 값을 덮어씁니다.
+function addPropertyAndValue(obj, property, value) {
+    obj[property] = value;
+}
