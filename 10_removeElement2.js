289d7b20f978c455612f7e9d81bfedce10819e12
admin2 <kms30922@naver.com>
2023-01-27 11:37:00 +0900

    coplit-고차함수

---
+++ 10_removeElement2.js
@@ -0,0 +1,13 @@
+//배열과 임의의 값(discarder)을 입력받아 기존 배열에서 discarder와 일치하는 요소가 제거된 새로운 배열을 리턴
+//반복문(for, while) 사용은 금지
+//arr.indexOf, arr.splice, arr.slice, arr.forEach 등의 사용은 금지
+//arr.filter를 이용
+//입력받은 배열을 수정하지 않아야 합니다.
+//빈 배열을 입력받은 경우, 빈 배열을 리턴
+function removeElement(arr, discarder) {
+    if (arr === []) {
+        return [];
+      }
+      let result = arr.filter(word => word !== discarder);
+      return result;
+}
