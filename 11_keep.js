1a731b0e9b39d4155286040d9f34dfef6c7e4c4a
admin2 <kms30922@naver.com>
2023-01-30 09:37:16 +0900

    coplit-고차함수

---
+++ 11_keep.js
@@ -0,0 +1,15 @@
+//배열과 임의의 값(keeper)을 입력받아 기존 배열에서 keeper와 일치하는 요소만을 갖는 새로운 배열을 리턴
+//반복문(for, while) 사용은 금지
+//arr.indexOf, arr.splice, arr.slice, arr.forEach 등의 사용은 금지
+//arr.filter를 이용
+//입력받은 배열을 수정하지 않아야 합니다.
+//빈 배열을 입력받은 경우, 빈 배열을 리턴
+//keeper와 일치하는 요소가 없는 경우, 빈 배열을 리턴
+function keep(arr, keeper) {
+  return arr.filter(function (el) {
+    if (el === keeper) {
+      return true;
+    }
+    return false;
+  });
+}
