60933c2f25d41df7fffd5c975ff05ee1e740a315
admin2 <kms30922@naver.com>
2023-01-31 11:49:15 +0900

    coplit-고차함수

---
+++ 12_filterOddLengthWords.js
@@ -0,0 +1,10 @@
+//문자열을 요소로 갖는 배열을 입력받아 그 길이가 홀수인 요소만을 갖는 배열을 리턴
+//반복문(for, while) 사용은 금지
+//arr.indexOf, arr.splice, arr.slice, arr.forEach 등의 사용은 금지
+//arr.filter를 이용
+//빈 배열을 입력받은 경우, 빈 배열을 리턴
+function filterOddLengthWords(words) {
+    return words.filter(function (el) {
+        return el.length % 2 !== 0;
+    });
+}
