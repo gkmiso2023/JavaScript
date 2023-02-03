ea4bbe97a3030a6c180263c7998e627118126070
admin2 <kms30922@naver.com>
2023-02-03 16:26:44 +0900

    coplit-고차함수

---
+++ 23_computeSumOfAllElements.js
@@ -0,0 +1,9 @@
+//number 타입을 요소로 갖는 배열을 입력받아 배열의 모든 요소의 합을 리턴
+//반복문(for, while) 사용은 금지
+//arr.reduce를 이용
+function computeSumOfAllElements(arr) {
+    if (arr.length === 0) return 0;
+    return arr.reduce(function (acc, val) {
+        return acc + val;
+    });
+}
