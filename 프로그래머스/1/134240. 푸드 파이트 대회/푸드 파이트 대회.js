c62efbc45282a93744468d1508d74d1b7481209f
miso-hub <120363595+miso-hub@users.noreply.github.com>
2024-01-29 12:07:46 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,9 @@
+function solution(food) {
+  let result = "";
+
+  for (let i = 1; i < food.length; i++) {
+    result += String(i).repeat(Math.floor(food[i] / 2));
+  }
+
+  return result + "0" + [...result].reverse().join("");
+}
